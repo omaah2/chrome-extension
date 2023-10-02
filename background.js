a4bb21919abbafba573eaba7c308125379b3a644
omaah2 <ummkhaladoluwatosin@gmail.com>
2023-10-02 16:57:15 +0100

    ...

--- background.js
+++ background.js
@@ -1,32 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.onConnect.addListener((port) => {
-    port.postMessage({ action: "popup-connected" });
 
-    port.onMessage.addListener((msg) => {
-      if (msg.action === "request-permissions") {
-        chrome.permissions.request(
-          {
-            permissions: ["activeTab", "videoCapture"],
-          },
-          (granted) => {
-            port.postMessage({ permissionsGranted: granted });
-          }
-        );
-      } else if (msg.action === "start-recording") {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "start-recording" });
-        });
-      } else if (msg.action === "stop-recording") {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.tabs.sendMessage(
-            tabs[0].id,
-            { action: "stop-recording" },
-            (response) => {
-              port.postMessage({ url: response.url });
-            }
-          );
-        });
-      }
-    });
-  });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
+        files: ["content.js"],
+      })
+      .then(() => {
+        console.log("injected content script");
+      })
+      .catch((err) => console.log(err, "error in the background script"));
+  }
 });
--- popUp.js
+++ popUp.js
@@ -1,5 +1,36 @@
-import { createRoot } from 'react-dom';
-import React from 'react';
-import popUp from "./src/pop/popUp";
+document.addEventListener("DOMContentLoaded", () => {
+  const startRecordingButton = document.querySelector("button#start_recording");
+  const stopRecordingButton = document.querySelector("button#stop_recording");
 
-createRoot(document.getElementById('root')).render(<popUp />);
+  startRecordingButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "request_recording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 14");
+          }
+        }
+      );
+    });
+  });
+
+  stopRecordingButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: "stoprecording" },
+        function (response) {
+          if (!chrome.runtime.lastError) {
+            console.log(response);
+          } else {
+            console.log(chrome.runtime.lastError, "error line 27");
+          }
+        }
+      );
+    });
+  });
+});
